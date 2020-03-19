91bf929ed6ac641273f64138812faddda76ccd04
Kitch04 <55270495+Kitch04@users.noreply.github.com>
2020-03-19 18:34:09 +0800

    Update table.php

--- table.php
+++ table.php
@@ -46,5 +46,13 @@ $list = $data['records'];
 			<?php } ?>
 		</tbody>
 </table>	
-<label> gago ka</label>
+<a href="index.php" onclick="signOut();"><button>Sign out</button></a>
+<script>
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  }
+</script>
 
